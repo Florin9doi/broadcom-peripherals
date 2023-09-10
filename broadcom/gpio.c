44f5392759c572502e3707e95ab2dfd68fb184da
Florin9doi <Florin9doi@users.noreply.github.com>
2023-09-10 18:22:01 +0300

    bcm2835/7 GPIO pull-up/down

--- broadcom/gpio.c
+++ broadcom/gpio.c
@@ -17,16 +17,36 @@ BP_Function_Enum FSEL_VALUES[6] = {
 };
 
 BP_PULL_Enum gpio_get_pull(uint8_t pin_number) {
+    #if BCM_VERSION == 2711
     volatile uint32_t* pupd = &GPIO->GPIO_PUP_PDN_CNTRL_REG0;
     return (pupd[pin_number / 16] >> ((pin_number % 16) * 2)) & 0x3;
+    #else
+    /*
+    TODO:
+    is not possible to read back the current Pull-up/down settings
+    it is the user's responsibility to remember which pull-up/downs are active
+    */
+    return 0;
+    #endif
 }
 
 void gpio_set_pull(uint8_t pin_number, BP_PULL_Enum pull) {
+    #if BCM_VERSION == 2711
     volatile uint32_t* pupd = &GPIO->GPIO_PUP_PDN_CNTRL_REG0;
     uint32_t shift = (pin_number % 16) * 2;
     uint32_t mask = 0x3 << shift;
     uint32_t current = pupd[pin_number / 16];
     pupd[pin_number / 16] = (current & ~mask) | (pull & 0x3) << shift;
+    #else
+    volatile uint32_t* pudclk = &GPIO->GPPUDCLK0;
+    GPIO->GPPUD = pull;
+    for (int i = 0; i < 150; i++);
+    pudclk[pin_number / 32] = 1 << (pin_number % 32);
+    for (int i = 0; i < 150; i++);
+    GPIO->GPPUD = 0;
+    GPIO->GPPUDCLK0 = 0;
+    GPIO->GPPUDCLK1 = 0;
+    #endif
 }
 
 BP_Function_Enum gpio_get_function(uint8_t pin_number) {
@@ -56,5 +76,5 @@ void gpio_set_value(uint8_t pin_number, bool value) {
     } else {
         lev = &GPIO->GPCLR0;
     }
-    lev[pin_number / 32] = 1 << (pin_number % 32);  
+    lev[pin_number / 32] = 1 << (pin_number % 32);
 }
